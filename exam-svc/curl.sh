82f243c07f97b8841e51437e8e92f8afd50dacdf
Nawaphong Sitaruno <nawaphong.s@arise.tech>
2025-11-18 19:35:28 +0700

    update

--- exam-svc/curl.sh
+++ exam-svc/curl.sh
@@ -9,10 +9,21 @@ curl -X POST http://localhost:8000/api/v1/order/create \
   -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpbnRlcm5hbCIsImlhdCI6MTc2MzQwOTEwNiwiZXhwIjoxNzk0OTQ5MTk3LCJhdWQiOiJ3d3cuZXhhbXBsZS5jb20iLCJzdWIiOiJqcm9ja2V0QGV4YW1wbGUuY29tIn0.DYXfoe420o-mJhmHT1HkpcShdY_ipOYZu87fSA-GoG4"
 
 
-curl -X POST http://localhost:8080/api/v1/order/cancel
+curl -X POST http://localhost:8000/api/v1/order/cancel
 
-curl -X POST http://localhost:8081/api/v1/user/login
-curl -X GET http://localhost:8081/api/v1/user/get-profile
+curl -X POST http://localhost:8000/api/v1/user/login
+curl -X GET http://localhost:8000/api/v1/user/get-profile
+curl -X GET http://localhost:8000/api/v1/user/get-profile \
+  -H "x-api-key: test"
+curl -X GET http://localhost:8000/api/v1/user/get-profile \
+  -H "x-api-key: test2"
+curl -X GET http://localhost:8000/api/v1/user/get-profile \
+  -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpbnRlcm5hbCIsImlhdCI6MTc2MzQ2Nzc5NiwiZXhwIjoxNzk1MDAzNzk2LCJhdWQiOiJ3d3cuZXhhbXBsZS5jb20iLCJzdWIiOiJqcm9ja2V0QGV4YW1wbGUuY29tIn0.RpScf31cDGWEqDpsZBnhTVS-aWX_u1AVCJ-rGAGCMXY"
 
-curl -X POST http://localhost:8082/api/v1/payment/payment
+curl -X POST http://localhost:8000/api/v1/payment/payment \
+  -H "x-api-key: test"
+curl -X POST http://localhost:8000/api/v1/payment/payment \
+  -H "x-api-key: test2"
+
+curl -X POST http://localhost:8000/api/v1/payment/payment
 
